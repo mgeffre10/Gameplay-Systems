42165f6018b3defad7cc03104d410e14fcdf3b54
Michael Geffre <geffrejunior92@gmail.com>
2020-07-19 20:29:07 -0700

    Initial Setup

---
+++ CPP_Projects/TaskSystem/Source/TaskSystem/TSGameInstance.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "TSGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TASKSYSTEM_API UTSGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+
+public:
+
+	// Task System
+};
---
+++ CPP_Projects/TaskSystem/Source/TaskSystem/TaskManagerSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "TaskManagerSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TASKSYSTEM_API UTaskManagerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
